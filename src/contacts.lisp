4100df293fae8718de75fb79bdc224126797822d
Elias Martenson <elias.martenson@murex.com>
2011-11-16 09:39:34 +0800

    Updates to email address node now works.
    --HG--
    branch : atom_updates_development

--- src/contacts.lisp
+++ src/contacts.lisp
@@ -38,7 +38,7 @@
                  :node "gd:name/gd:familyName/text()"
 		 :documentation "Content of the <gd:name><gd:familyName> node")
    (email        :type list
-		 :reader contact-email
+		 :accessor contact-email
                  :node ("gd:email" "@rel" "@address" ("@primary" :true-false))
                  :node-collectionp t
                  :node-clear-function clear-email
@@ -58,7 +58,7 @@
                       :session session))
 
 #|
-  Test code:
+Test code:
 
 (with-gdata-namespaces
                  (let ((doc (cxml-dom:create-document)))
@@ -66,5 +66,19 @@
                          (entry-node (dom:import-node doc (feed-entry-node-dom *contact*) t)))
                      (dom:append-child root-node entry-node)
                      (dom:append-child doc root-node)
+                     (dom:map-document (cxml:make-character-stream-sink *standard-output*) doc))))
+
+With header copy:
+
+(with-gdata-namespaces
+                 (let ((doc (cxml-dom:create-document)))
+                   (let ((root-node (dom:create-element-ns doc (find-namespace-url "atom") "root-node"))
+                         (entry-node (dom:import-node doc (feed-entry-node-dom *contact*) t)))
+                     (dom:append-child root-node entry-node)
+                     (dom:append-child doc root-node)
+                     (dom:map-node-map #'(lambda (a) (dom:set-attribute (dom:document-element doc)
+                                                                      (dom:node-name a)
+                                                                      (dom:node-value a)))
+                                     (dom:attributes (dom:document-element (dom:owner-document *n*))))
                      (dom:map-document (cxml:make-character-stream-sink *standard-output*) doc))))
 |#
